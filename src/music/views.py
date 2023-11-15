680308d3846d2b9552cdf1ee3f1adb8a37cd5817
Hailemichael Atrsaw <hailat.alx@gmail.com>
2023-11-15 11:28:04 +0300

    pagination set to 10

--- src/music/views.py
+++ src/music/views.py
@@ -10,7 +10,7 @@ from .serializers import *
 
 
 class StandardResultsSetPagination(PageNumberPagination):
-    page_size = 2
+    page_size = 10
     page_size_query_param = 'page_size'
 
 class ArtistsViewSet(viewsets.ModelViewSet):
