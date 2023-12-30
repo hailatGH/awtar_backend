1b92fe314e6d5d9eb5ef14c5b19dc0ceea8592b7
Sabian-A <abelteum13@gmail.com>
2023-12-30 16:35:50 +0300

    addesd search routers

---
+++ src/search/router.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+
+from search.views import *
+
+searchrouter = DefaultRouter(trailing_slash=False)
+searchrouter.register(r'track',  TrackViewSet, basename="track")
+searchrouter.register(r'album',  AlbumViewSet, basename="album")
+searchrouter.register(r'artist',  ArtistViewSet, basename="artist")
