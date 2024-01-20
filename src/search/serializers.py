d7d18ca80f250aa752086fd4be83cd99be028a3c
Sabian-A <abelteum13@gmail.com>
2024-01-20 20:21:26 +0300

    fix

--- src/search/serializers.py
+++ src/search/serializers.py
@@ -9,10 +9,19 @@ class ArtistsSerializer(serializers.ModelSerializer):
     class Meta:
         model = ArtistsModel
         fields = '__all__'
-        
     def get_artist_profileImage(self, obj):
-        artist_profileImage = obj.artist_profileImage
-        # artist_profileImage = "https://zemamultimediablobcdn.azureedge.net/zemacontainer/" + artist_profileImage # replace adjust_artist_profileImage with your custom function
+        request = self.context.get('request')
+        if obj.artist_profileImage:
+            image_url = obj.artist_profileImage.url
+            if request is not None:
+                current_site = Site.objects.get_current(request=request)
+                image_url = current_site.domain + image_url
+            return image_url
+        else:
+            return None
+    def get_artist_profileImage(self, obj):
+        artist_profileImage = obj.artist_profileImage.url
+        # artist_profileImage = "https://awtarstoragev100.blob.core.windows.net/awtarstorage/media" + artist_profileImage # replace adjust_artist_profileImage with your custom function
         return artist_profileImage
     
     def get_number_of_albums(self,obj):
